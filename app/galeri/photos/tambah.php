d1586a92045df28b4734f49e61764517afd9c78c
davi <davifahreza14@gmail.com>
2025-02-11 12:49:52 +0700

    Benerin directory

--- app/galeri/photos/tambah.php
+++ app/galeri/photos/tambah.php
@@ -7,15 +7,17 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $photoDesc  = $_POST['photo_desc'];
     $userId     = $_SESSION['user_id'];
     $albumId = $_POST['album_id'];
-    
+
     $photoFile     = $_FILES['photo_file'];
     $photoFileName = $photoFile['name'];
 
     $targetDir = '../../../galeri/';
 
-    move_uploaded_file($photoFile['tmp_name'], $targetDir . $photoFileName);
+    if(!is_dir($targetDir)){
+        mkdir($targetDir);
+    }
 
-    
+    move_uploaded_file($photoFile['tmp_name'], $targetDir . $photoFileName);
 
     $query = "INSERT INTO photos (photo_file, photo_title, photo_desc, users_id, albums_id, created_at)
               VALUES ('$photoFileName', '$photoTitle', '$photoDesc', '$userId', '$albumId', NOW())";
